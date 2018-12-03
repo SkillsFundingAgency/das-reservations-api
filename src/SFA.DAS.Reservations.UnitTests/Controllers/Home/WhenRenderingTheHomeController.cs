53bef921cd9a3659ffab3f53b6e8d03ae40df972
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2018-12-03 10:22:06 +0000

    Add test project

---
+++ src/SFA.DAS.Reservations.UnitTests/Controllers/Home/WhenRenderingTheHomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Reservations.Web.Controllers;
+
+namespace SFA.DAS.Reservations.UnitTests.Controllers.Home
+{
+    public class WhenRenderingTheHomeController
+    {
+        private HomeController _homeController;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _homeController = new HomeController();
+        }
+
+        [Test]
+        public async Task Then_The_Correct_View_Is_Displayed()
+        {
+            //Act
+            var actual = await _homeController.Index();
+
+            //Assert
+            Assert.IsNotNull(actual);
+            var result = actual as ViewResult;
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
--- src/SFA.DAS.Reservations/Controllers/HomeController.cs
+++ src/SFA.DAS.Reservations/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.Reservations.Web.Models;
 
@@ -6,15 +7,10 @@ namespace SFA.DAS.Reservations.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View("Index");
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
