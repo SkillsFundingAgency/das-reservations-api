0b5957a22f9cf02c932c2ac23f2e62d9754c4bb8
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-01-30 09:50:52 +0000

    Move AzureAdScopeClaimTransformation
    Moved class to infrastructure

--- src/SFA.DAS.Reservations.Api/Startup.cs
+++ src/SFA.DAS.Reservations.Api/Startup.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -145,32 +141,5 @@ namespace SFA.DAS.Reservations.Api
                     template: "api/{controller=Reservation}/{action=Index}/{id?}");
             });
         }
-    }
-
-    public class AzureAdScopeClaimTransformation : IClaimsTransformation
-    {
-        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
-        {
-
-            var scopeClaims = principal.FindAll(Constants.ScopeClaimType).ToList();
-            if (scopeClaims.Count != 1 || !scopeClaims[0].Value.Contains(' '))
-            {
-                // Caller has no scopes or has multiple scopes (already split)
-                // or they have only one scope
-                return Task.FromResult(principal);
-            }
-
-            Claim claim = scopeClaims[0];
-            string[] scopes = claim.Value.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            IEnumerable<Claim> claims = scopes.Select(s => new Claim(Constants.ScopeClaimType, s));
-
-            return Task.FromResult(new ClaimsPrincipal(new ClaimsIdentity(principal.Identity, claims)));
-        }
-    }
-
-    public static class Constants
-    {
-        public const string ScopeClaimType = "http://schemas.microsoft.com/identity/claims/scope";
-        public const string ObjectIdClaimType = "http://schemas.microsoft.com/identity/claims/objectidentifier";
     }
 }
