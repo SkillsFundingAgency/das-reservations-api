7e9e161884375afb6348d8c75baa76ad2fee4cbc
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-04-25 19:43:51 +0100

    Add query handler for AvailableDates
    Added new query handler for available dates that calls the
    AvailableDates service

---
+++ src/SFA.DAS.Reservations.Application/Rules/Queries/GetAvailableDatesQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Options;
+using SFA.DAS.Reservations.Domain.Configuration;
+using SFA.DAS.Reservations.Domain.Rules;
+
+namespace SFA.DAS.Reservations.Application.Rules.Queries
+{
+    public class GetAvailableDatesQueryHandler : IRequestHandler<GetAvailableDatesQuery, GetAvailableDatesResult>
+    {
+        private readonly IAvailableDatesService _availableDatesService;
+        
+        public GetAvailableDatesQueryHandler(IAvailableDatesService availableDatesService)
+        {
+            _availableDatesService = availableDatesService;
+            
+        }
+
+        public async Task<GetAvailableDatesResult> Handle(GetAvailableDatesQuery request, CancellationToken cancellationToken)
+        {
+            var availableDates = _availableDatesService.GetAvailableDates();
+
+            return new GetAvailableDatesResult
+            {
+                AvailableDates = availableDates
+            };
+        }
+    }
+}
