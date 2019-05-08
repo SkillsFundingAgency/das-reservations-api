79486e8d79b67f9adfced3b18f1090cf42b848f4
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-05-08 11:05:30 +0100

    Create new messages project

---
+++ src/SFA.DAS.Reservations.Messages/ReservationCreatedEvent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SFA.DAS.Reservations.Domain.Reservations;
+
+namespace SFA.DAS.Reservations.Messages
+{
+    public class ReservationCreatedEvent
+    {
+        public ReservationCreatedEvent(Reservation reservation)
+        {
+            if (reservation == null)
+            {
+                return;
+            }
+
+            Id = reservation.Id;
+            AccountLegalEntityId = reservation.AccountLegalEntityId;
+            AccountLegalEntityName = reservation.AccountLegalEntityName;
+            CourseId = reservation.Course.CourseId;
+            StartDate = reservation.StartDate;
+            CourseName = reservation.Course.Title;
+            EndDate = reservation.ExpiryDate;
+            CreatedDate = reservation.CreatedDate;
+        }
+
+        public Guid Id { get; }
+        public long AccountLegalEntityId { get; }
+        public string AccountLegalEntityName { get;  }
+        public string CourseId { get; }
+        public DateTime StartDate { get; }
+        public string CourseName { get; }
+        public DateTime EndDate { get;  }
+        public DateTime CreatedDate { get;  }
+    }
+}
