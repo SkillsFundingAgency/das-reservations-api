898c08e981031b049807b09aa140214a9c19d3ca
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-05-13 15:13:42 +0100

    Add acount legal entity repository

---
+++ src/SFA.DAS.Reservations.Data.UnitTests/Repository/WhenGettingAccountLegalEntitiesFromTheRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Reservations.Data.Repository;
+using SFA.DAS.Reservations.Data.UnitTests.DatabaseMock;
+using SFA.DAS.Reservations.Domain.Entities;
+
+namespace SFA.DAS.Reservations.Data.UnitTests.Repository
+{
+    public class WhenGettingAccountLegalEntitiesFromTheRepository
+    {
+        private Mock<IReservationsDataContext> _reservationsDataContext;
+        private AccountLegalEntityRepository _legalEntityRepository;
+        private List<AccountLegalEntity> _legalEntities;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _legalEntities = new List<AccountLegalEntity>
+            {
+                new AccountLegalEntity
+                {
+                    Id = Guid.NewGuid(),
+                    AccountId = 1,
+                    AccountLegalEntityName = "Test"
+                },
+                new AccountLegalEntity
+                {
+                    Id = Guid.NewGuid(),
+                    AccountId = 1,
+                    AccountLegalEntityName = "Test 1"
+                },
+                new AccountLegalEntity
+                {
+                    Id = Guid.NewGuid(),
+                    AccountId = 2,
+                    AccountLegalEntityName = "Test 2"
+                }
+            };
+
+            _reservationsDataContext = new Mock<IReservationsDataContext>();
+            _reservationsDataContext.Setup(x => x.AccountLegalEntities).ReturnsDbSet(_legalEntities);
+
+            _legalEntityRepository = new AccountLegalEntityRepository(_reservationsDataContext.Object);
+        }
+
+        [Test]
+        public async Task Then_The_LegalEntities_Are_Returned_By_AccountId()
+        {
+            //Act
+            var actual = await _legalEntityRepository.GetByAccountId(1);
+
+            //Assert
+            Assert.IsAssignableFrom<List<AccountLegalEntity>>(actual);
+            Assert.IsNotEmpty(actual);
+            Assert.AreEqual(2, actual.Count);
+        }
+
+        [Test]
+        public async Task Then_If_There_Are_No_Results_An_Empty_List_Is_Returned()
+        {
+            //Act
+            var actual = await _legalEntityRepository.GetByAccountId(3);
+
+            //Assert
+            Assert.IsEmpty(actual);
+        }
+    }
+}
---
+++ src/SFA.DAS.Reservations.Data/Repository/AccountLegalEntityRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SFA.DAS.Reservations.Domain.Entities;
+
+namespace SFA.DAS.Reservations.Data.Repository
+{
+    public class AccountLegalEntityRepository
+    {
+        private readonly IReservationsDataContext _reservationsDataContext;
+
+        public AccountLegalEntityRepository(IReservationsDataContext reservationsDataContext)
+        {
+            _reservationsDataContext = reservationsDataContext;
+        }
+
+        public async Task<IList<AccountLegalEntity>> GetByAccountId(long accountId)
+        {
+            var legalEntities = await _reservationsDataContext.AccountLegalEntities.Where(c => c.AccountId.Equals(accountId)).ToListAsync();
+
+            return legalEntities;
+
+        }
+    }
+}
