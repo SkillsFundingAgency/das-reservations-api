92f10bb55acdf579cc360cdc901504f0b4e718db
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-05-13 10:42:41 +0100

    Add new table entity for AccountLegalEntity

---
+++ src/SFA.DAS.Reservations.Database/Tables/AccountLegalEntity.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[AccountLegalEntity]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+	[AccountId] BIGINT NOT NULL,
+	[LegalEntityId] BIGINT NOT NULL,
+	[Name] VARCHAR(250) not null,
+)
