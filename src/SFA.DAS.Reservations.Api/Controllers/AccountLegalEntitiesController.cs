66451617bc73da2138d604739f502839d26f3559
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-05-14 10:25:28 +0100

    Implement controller to get list of LegalEntities

---
+++ src/SFA.DAS.Reservations.Api/Controllers/AccountLegalEntitiesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Reservations.Api.Models;
+using SFA.DAS.Reservations.Application.AccountLegalEntities.Queries;
+
+namespace SFA.DAS.Reservations.Api.Controllers
+{
+    [Route("api/[controller]/")]
+    [ApiController]
+    public class AccountLegalEntitiesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AccountLegalEntitiesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [Route("{accountId")]
+        public async Task<IActionResult> GetByAccountId(long accountId)
+        {
+            try
+            {
+                var response = await _mediator.Send(new GetAccountLegalEntitiesQuery { AccountId = accountId });
+                return Ok(response.AccountLegalEntities);
+            }
+            catch (ArgumentException e)
+            {
+                return BadRequest(new ArgumentErrorViewModel
+                {
+                    Message = e.Message,
+                    Params = e.ParamName
+                });
+            }
+        }
+    }
+}
