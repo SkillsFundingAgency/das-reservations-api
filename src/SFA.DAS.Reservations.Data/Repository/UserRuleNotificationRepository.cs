3ee26feafdaf9a82fb397048bda2fd7221a8142e
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-05-29 10:51:56 +0100

    WIP repository Add UserRuleNotification

---
+++ src/SFA.DAS.Reservations.Data/Repository/UserRuleNotificationRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using SFA.DAS.Reservations.Domain.Entities;
+
+namespace SFA.DAS.Reservations.Data.Repository
+{
+    public class UserRuleNotificationRepository
+    {
+        private readonly IReservationsDataContext _reservationsDataContext;
+
+        public UserRuleNotificationRepository(IReservationsDataContext reservationsDataContext)
+        {
+            _reservationsDataContext = reservationsDataContext;
+        }
+
+        public async Task<UserRuleNotification> Add(UserRuleNotification userRuleNotification)
+        {
+            var result = await _reservationsDataContext.UserRuleNotifications.AddAsync(userRuleNotification);
+
+            _reservationsDataContext.SaveChanges();
+
+            return result.Entity;
+        }
+    }
+}
