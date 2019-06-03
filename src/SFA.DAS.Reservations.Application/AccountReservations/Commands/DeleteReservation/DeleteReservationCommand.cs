8267fbf299a3b5ec75ddc5af76022e8b06c57341
Scott Wakefield <scott@expertcc.co.uk>
2019-06-03 13:56:43 +0100

    MF-155 delete command handler.

--- src/SFA.DAS.Reservations.Application/AccountReservations/Commands/DeleteReservation/DeleteReservationCommand.cs
+++ src/SFA.DAS.Reservations.Application/AccountReservations/Commands/DeleteReservation/DeleteReservationCommand.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using MediatR;
 
 namespace SFA.DAS.Reservations.Application.AccountReservations.Commands.DeleteReservation
 {
-    public class DeleteReservationCommand
+    public class DeleteReservationCommand : IRequest<Unit>
     {
         public Guid ReservationId { get; set; }
     }
