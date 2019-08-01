543ee5100ccd11410f8d6175e086bf0d2642e9f0
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-08-01 21:48:00 +0100

    Add index to AccountLegalEntity table
    Added index for AccountLegalEntityId on the AccountLegalEntity table

--- src/SFA.DAS.Reservations.Database/Tables/AccountLegalEntity.sql
+++ src/SFA.DAS.Reservations.Database/Tables/AccountLegalEntity.sql
@@ -10,3 +10,8 @@
     [IsLevy] BIT NOT NULL DEFAULT 0, 
     [AgreementType] TINYINT NOT NULL DEFAULT 0
 )
+GO;
+
+CREATE NONCLUSTERED INDEX [IDX_AccountLegalEntity_AccountLegalEntityId] ON [dbo].[AccountLegalEntity] (AccountLegalEntityId) 
+INCLUDE (Id,[AccountId],[LegalEntityId],[AccountLegalEntityName],[ReservationLimit],[AgreementSigned],[IsLevy],[AgreementType]) WITH (ONLINE = ON) 
+GO;
