3f5e283197d2191784ba0c8cfa4543389bfa3371
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-09-09 11:39:30 +0100

    Move acceptance tests
    Moved the acceptance tests to a folder, added some extra create
    reservation scenarios. Added placeholder for available dates

---
+++ src/SFA.DAS.Reservations.Api.AcceptanceTests/Steps/StepsBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using SFA.DAS.Common.Domain.Types;
+using SFA.DAS.Reservations.Data;
+using SFA.DAS.Reservations.Domain.Entities;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Reservations.Api.AcceptanceTests.Steps
+{
+    public class StepsBase
+    {
+        protected readonly TestData TestData;
+        protected readonly IServiceProvider Services;
+
+        public StepsBase(TestData testData, TestServiceProvider serviceProvider)
+        {
+            TestData = testData;
+            Services = serviceProvider;
+        }
+
+
+        [BeforeScenario]
+        public void InitialiseTestDatabaseData()
+        {
+            var dbContext = Services.GetService<ReservationsDataContext>();
+
+            TestData.Course = new Course
+            {
+                CourseId = "234",
+                Level = 1,
+                Title = "Tester"
+            };
+
+            dbContext.Courses.Add(TestData.Course);
+
+            TestData.AccountLegalEntity = new AccountLegalEntity
+            {
+                AccountId = 1,
+                AccountLegalEntityId = 1,
+                AccountLegalEntityName = "Test Corp",
+                AgreementType = AgreementType.NonLevyExpressionOfInterest,
+                AgreementSigned = true
+            };
+
+            dbContext.AccountLegalEntities.Add(TestData.AccountLegalEntity);
+
+            dbContext.SaveChanges();
+        }
+    }
+}
