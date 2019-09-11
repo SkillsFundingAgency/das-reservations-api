2a37b193c3df763c6c96c685a3c40f4c9c6fa8f4
Robin O'Connor <github@rocketmansoftware.com>
2019-09-11 17:34:11 +0100

    Added two tests to check that the correct available dates are being returned

---
+++ src/SFA.DAS.Reservations.Api.AcceptanceTests/Steps/AvailableDatesSteps.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Reservations.Data;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using SFA.DAS.Common.Domain.Types;
+using SFA.DAS.Reservations.Api.Controllers;
+using SFA.DAS.Reservations.Application.Rules.Queries;
+using SFA.DAS.Reservations.Domain.Rules;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Reservations.Api.AcceptanceTests.Steps
+{
+    [Binding]
+    public class AvailableDatesSteps: StepsBase
+    {
+        private const int DefaultAvailableDatesMonthCount = 6;
+        private static readonly DateTime EoiDefaultMinDate = new DateTime(2019, 08, 1);
+        private static readonly DateTime EoiDefaultMaxDate = new DateTime(2019, 12, 1);
+
+        private GetAvailableDatesResult _availableDatesResult;
+
+        public AvailableDatesSteps(TestData testData, TestServiceProvider serviceProvider) : base(testData, serviceProvider)
+        {
+        }
+
+        [Given(@"I have signed an EOI Agreement")]
+        public void GivenIHaveSignedAnEoiAgreement()
+        {
+            var dbContext = Services.GetService<ReservationsDataContext>();
+
+            var legalEntity = dbContext.AccountLegalEntities.Single(le => le.AccountLegalEntityId.Equals(TestData.AccountLegalEntity.AccountLegalEntityId));
+
+            legalEntity.AgreementType = AgreementType.NonLevyExpressionOfInterest;
+
+            dbContext.SaveChanges();
+        }
+        
+        [Given(@"I have not signed an EOI Agreement")]
+        public void GivenIHaveNotSignedAnEoiAgreement()
+        {
+            var dbContext = Services.GetService<ReservationsDataContext>();
+
+            var legalEntity = dbContext.AccountLegalEntities.Single(le => le.AccountLegalEntityId.Equals(TestData.AccountLegalEntity.AccountLegalEntityId));
+
+            legalEntity.AgreementType = AgreementType.Levy;
+
+            dbContext.SaveChanges();
+        }
+        
+        [When(@"I get available dates")]
+        public void WhenIGetAvailableDates()
+        {
+            var controller = Services.GetService<RulesController>();
+
+            var result = controller.GetAvailableDates(TestData.AccountLegalEntity.AccountLegalEntityId).Result as OkObjectResult;
+
+            _availableDatesResult = result?.Value as GetAvailableDatesResult;
+        }
+        
+        [Then(@"I should get EOI available dates back")]
+        public void ThenIShouldGetEoiAvailableDatesBack()
+        {
+            Assert.AreEqual(5, _availableDatesResult.AvailableDates.Count());
+
+            var expectedDates = GetExpectedDates(DefaultAvailableDatesMonthCount, EoiDefaultMinDate,  EoiDefaultMaxDate);
+
+            _availableDatesResult.AvailableDates.Should().BeEquivalentTo(expectedDates);
+        }
+        
+        [Then(@"I should get standard available dates back")]
+        public void ThenIShouldGetStandardAvailableDatesBack()
+        {
+            Assert.AreEqual(6, _availableDatesResult.AvailableDates.Count());
+
+            var expectedDates = GetExpectedDates(DefaultAvailableDatesMonthCount, DateTime.Now,  DateTime.MaxValue);
+
+            _availableDatesResult.AvailableDates.Should().BeEquivalentTo(expectedDates);
+        }
+
+        private static IEnumerable<AvailableDateStartWindow> GetExpectedDates(int expectedMonthCount, DateTime minDate, DateTime maxDate)
+        {
+            var expectedDates = new List<AvailableDateStartWindow>();
+
+            for (var index = 0; index < expectedMonthCount; index++)
+            {
+                var startDate = minDate.AddMonths(index);
+
+                if (startDate > maxDate)
+                    break;
+
+                var endDate = startDate.AddMonths(2);
+
+                var newDateWindow = new AvailableDateStartWindow
+                {
+                    StartDate = new DateTime(startDate.Year, startDate.Month, 1),
+                    EndDate = new DateTime(endDate.Year, endDate.Month,
+                        DateTime.DaysInMonth(endDate.Year, endDate.Month))
+                };
+
+                expectedDates.Add(newDateWindow);
+            }
+
+            return expectedDates;
+        }
+    }
+}
