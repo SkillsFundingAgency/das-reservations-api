007af536e9f76f0e9f20476199088ab53fd207a4
Scott Wakefield <scott@expertcc.co.uk>
2019-09-12 11:10:10 +0100

    MF-296 added empty steps.

---
+++ src/SFA.DAS.Reservations.Api.AcceptanceTests/Steps/ValidateReservationSteps.cs
@@ -0,0 +1,13 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Reservations.Api.AcceptanceTests.Steps
+{
+    [Binding]
+    public class ValidateReservationSteps : StepsBase
+    {
+        public ValidateReservationSteps(TestData testData, TestServiceProvider serviceProvider) 
+            : base(testData, serviceProvider)
+        {
+        }
+    }
+}
