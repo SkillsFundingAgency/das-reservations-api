e5b6b9e3aaa8d587832ea68036908afb91a07365
Robin O'Connor <github@rocketmansoftware.com>
2019-10-10 17:24:55 +0100

    Added new database table for holding provider permissions

---
+++ src/SFA.DAS.Reservations.Database/Tables/ProviderPermission.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ProviderPermission]
+(
+	[AccountId] BIGINT NOT NULL , 
+    [AccountLegalEntityId] BIGINT NOT NULL, 
+    [Ukprn] BIGINT NOT NULL, 
+    [CanCreateCohort] BIT NOT NULL, 
+    PRIMARY KEY ([AccountId],[AccountLegalEntityId],[Ukprn])
+)
