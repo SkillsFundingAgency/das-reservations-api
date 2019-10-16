773467b513827824403cea0b7fe271804485aa55
Robin O'Connor <github@rocketmansoftware.com>
2019-10-16 11:08:24 +0100

    Added reservation update event and sorted out setting breaking AT tests

---
+++ src/SFA.DAS.Reservations.Messages/ReservationUpdatedEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SFA.DAS.Reservations.Domain.Reservations;
+
+namespace SFA.DAS.Reservations.Messages
+{
+    public class ReservationUpdatedEvent
+    {
+        public ReservationUpdatedEvent(Guid reservationId, ReservationStatus status)
+        {
+            Id = reservationId;
+            Status = status;
+        }
+
+        public Guid Id { get; set; }
+        public ReservationStatus Status { get; set; }
+    }
+}
