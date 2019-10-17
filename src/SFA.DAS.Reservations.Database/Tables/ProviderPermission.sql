e756921f8a0f657a00825388fcb439fbb9a5f71e
Robin O'Connor <github@rocketmansoftware.com>
2019-10-17 14:37:04 +0100

    Removed unneeded refactor log and added cluster indexes to new table

--- src/SFA.DAS.Reservations.Database/Tables/ProviderPermission.sql
+++ src/SFA.DAS.Reservations.Database/Tables/ProviderPermission.sql
@@ -6,3 +6,12 @@
     [CanCreateCohort] BIT NOT NULL, 
     PRIMARY KEY ([AccountId],[AccountLegalEntityId],[Ukprn])
 )
+GO;
+
+CREATE NONCLUSTERED INDEX [IDX_ProviderPermission_AccountId] ON [dbo].[ProviderPermission] (AccountId) 
+INCLUDE ([AccountLegalEntityId],[Ukprn],[CanCreateCohort]) WITH (ONLINE = ON) 
+GO;
+
+CREATE NONCLUSTERED INDEX [IDX_ProviderPermission_UkprnAccountLegalEntityId] ON [dbo].[ProviderPermission] ([Ukprn], [AccountLegalEntityId]) 
+INCLUDE ([AccountId],[CanCreateCohort]) WITH (ONLINE = ON) 
+GO;
