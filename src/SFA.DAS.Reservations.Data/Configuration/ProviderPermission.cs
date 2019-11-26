6d81fdfe924cd6bb494cb3074bd1b5a53202c66a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2019-11-26 10:03:04 +0000

    Add db mapping for provider permission
    Added the db mapping for the provider permission and add relation to account legal entites

---
+++ src/SFA.DAS.Reservations.Data/Configuration/ProviderPermission.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SFA.DAS.Reservations.Data.Configuration
+{
+    public class ProviderPermission : IEntityTypeConfiguration<Domain.Entities.ProviderPermission>
+    {
+        public void Configure(EntityTypeBuilder<Domain.Entities.ProviderPermission> builder)
+        {
+            builder.ToTable("ProviderPermission");
+            builder.HasKey(x => new {x.AccountId, x.UkPrn, x.AccountLegalEntityId});
+
+            builder.Property(x => x.AccountId).HasColumnName("AccountId").HasColumnType("bigint").IsRequired();
+            builder.Property(x => x.UkPrn).HasColumnName("UkPrn").HasColumnType("UkPrn").IsRequired();
+            builder.Property(x => x.AccountLegalEntityId).HasColumnName("AccountLegalEntityId").HasColumnType("bigint").IsRequired();
+            builder.Property(x => x.CanCreateCohort).HasColumnName("CanCreateCohort").HasColumnType("bit").IsRequired();
+            
+            builder.HasOne(c => c.AccountLegalEntity).WithMany(c => c.ProviderPermissions)
+                .HasForeignKey(c => c.AccountLegalEntityId);
+        }
+    }
+}
