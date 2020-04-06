479f1ae4583a1ada5b8ec886e65bf2fef5e92df3
Scott Wakefield <scott@expertcc.co.uk>
2020-04-06 16:06:09 +0100

    MF-507 added accounts and account legal entities for ac tests to run.

--- src/SFA.DAS.Reservations.Api.AcceptanceTests/Features/ChangeOfParty.feature
+++ src/SFA.DAS.Reservations.Api.AcceptanceTests/Features/ChangeOfParty.feature
@@ -3,7 +3,6 @@
 	as a levy or a non levy employer
 	I want my reservation to be carried over
 
-@ignore
 Scenario: Change employer, non-levy to non-levy
 	Given I have the following existing reservation:
 	| Account Legal Entity Id | Provider Id | Start Date | Created Date | Course Id | Status    | Is Levy Account |
@@ -16,20 +15,18 @@ Scenario: Change employer, non-levy to non-levy
 	| 1                       | 15214       | 2019-07-01 | 2019-01-01   | 1         | Confirmed | False           |
 	| 2                       | 15214       | 2019-07-01 | today        | 1         | Change    | False           |
 
-@ignore
 Scenario: Change employer, levy to levy
 	Given I have the following existing reservation:
 	| Account Legal Entity Id | Provider Id | Start Date | Created Date | Course Id | Status    | Is Levy Account |
-	| 10                      | 15214       | 2019-07-01 | 2019-01-01   | 1         | Confirmed | False           |
+	| 10                      | 15214       | 2019-07-01 | 2019-01-01   | 1         | Confirmed | True            |
 	And I want to change account legal entity to 20
 	When I call change of circumstances
 	Then an http status code of 200 is returned
 	And I have the following reservations:
 	| Account Legal Entity Id | Provider Id | Start Date | Created Date | Course Id | Status    | Is Levy Account |
-	| 10                      | 15214       | 2019-07-01 | 2019-01-01   | 1         | Confirmed | False           |
-	| 20                      | 15214       | 2019-07-01 | today        | 1         | Change    | False           |
-	
-@ignore
+	| 10                      | 15214       | 2019-07-01 | 2019-01-01   | 1         | Confirmed | True            |
+	| 20                      | 15214       | 2019-07-01 | today        | 1         | Change    | True            |
+
 Scenario: Change employer, non-levy to levy
 	Given I have the following existing reservation:
 	| Account Legal Entity Id | Provider Id | Start Date | Created Date | Course Id | Status    | Is Levy Account |
@@ -41,7 +38,8 @@ Scenario: Change employer, non-levy to levy
 	| Account Legal Entity Id | Provider Id | Start Date | Created Date | Course Id | Status    | Is Levy Account |
 	| 1                       | 15214       | 2019-07-01 | 2019-01-01   | 1         | Confirmed | False           |
 	| 20                      | 15214       | 2019-07-01 | today        | 1         | Change    | True            |
-
+		
+@ignore
 Scenario: Change employer, levy to non-levy - not supported
 	Given I have the following existing reservation:
 	| Account Legal Entity Id | Provider Id | Start Date | Created Date | Course Id | Status    | Is Levy Account |
--- src/SFA.DAS.Reservations.Api.AcceptanceTests/Steps/StepsBase.cs
+++ src/SFA.DAS.Reservations.Api.AcceptanceTests/Steps/StepsBase.cs
@@ -21,6 +21,7 @@ namespace SFA.DAS.Reservations.Api.AcceptanceTests.Steps
         protected readonly TestData TestData;
         protected readonly TestResults TestResults;
         protected readonly IServiceProvider Services;
+        private readonly ReservationsDataContext _dbContext;
 
         public StepsBase(TestData testData, TestResults testResults, TestServiceProvider serviceProvider)
         {
@@ -28,22 +29,51 @@ namespace SFA.DAS.Reservations.Api.AcceptanceTests.Steps
             TestResults = testResults;
             Services = serviceProvider;
             UserId = Guid.NewGuid();
+
+            _dbContext = Services.GetService<ReservationsDataContext>();
         }
-
-
-        [BeforeScenario()]
+        
+        [BeforeScenario]
         public void InitialiseTestDatabaseData()
         {
             ConfigureCustomValueRetrievers();
             ConfigureCustomValueComparers();
 
+            ArrangeTestData();
+
+            AddCoursesToDb();
+            AddAccountsToDb();
+            AddAccountLegalEntitiesToDb();
+        }
+
+        private void ConfigureCustomValueRetrievers()
+        {
+            Service.Instance.ValueRetrievers.Replace<DateTimeValueRetriever, CustomDateTimeValueRetriever>();
+            Service.Instance.ValueRetrievers.Replace<NullableDateTimeValueRetriever, CustomNullableDateTimeValueRetriever>();
+            Service.Instance.ValueRetrievers.Replace<ShortValueRetriever, CustomShortValueRetriever>();
+        }
+
+        private void ConfigureCustomValueComparers()
+        {
+            Service.Instance.ValueComparers.Register<CustomShortValueComparer>();
+            Service.Instance.ValueComparers.Register(new CustomDateTimeComparer(new CustomDateTimeValueRetriever()));
+        }
+
+        private void ArrangeTestData()
+        {
             TestData.Course = new Course
             {
                 CourseId = "1",
                 Level = 1,
                 Title = "Tester",
                 ReservationRule = new List<Rule>()
-            };           
+            };   
+            
+            TestData.Account = new Account
+            {
+                Id = AccountId,
+                Name = "Test Account"
+            };
 
             TestData.AccountLegalEntity = new AccountLegalEntity
             {
@@ -62,66 +92,80 @@ namespace SFA.DAS.Reservations.Api.AcceptanceTests.Steps
             };
             
             TestData.UserId = UserId;
+        }
 
-            var dbContext = Services.GetService<ReservationsDataContext>();
-
-            if (dbContext.Courses.Find(TestData.Course.CourseId) == null)
+        private void AddCoursesToDb()
+        {
+            if (_dbContext.Courses.Find(TestData.Course.CourseId) == null)
             {
-                dbContext.Courses.Add(TestData.Course);
-            }
-
-            var legalEntity = dbContext.AccountLegalEntities.SingleOrDefault(e => e.AccountLegalEntityId.Equals(TestData.AccountLegalEntity.AccountLegalEntityId));
-            
-            if (legalEntity == null)
-            {
-                dbContext.AccountLegalEntities.Add(TestData.AccountLegalEntity);
-
-                dbContext.SaveChanges();
-            }
-
-            var anotherAccountLegalEntity = new AccountLegalEntity
-            {
-                AccountId = AccountId + 1,
-                AccountLegalEntityId = AccountLegalEntityId + 100,
-                AccountLegalEntityName = "Pass Mark Corp",
-                AgreementSigned = true
-            };
-
-            legalEntity = dbContext.AccountLegalEntities.SingleOrDefault(e => e.AccountLegalEntityId.Equals(anotherAccountLegalEntity.AccountLegalEntityId));
-
-            if (legalEntity == null)
-            {
-                dbContext.AccountLegalEntities.Add(anotherAccountLegalEntity);
-
-                dbContext.SaveChanges();
-            }
-
-            TestData.Account = new Account
-            {
-                Id = AccountId,
-                Name = "Test Account"
-            };
-
-            var account = dbContext.Accounts.Find(AccountId);
-
-            if (account == null)
-            {
-                dbContext.Accounts.Add(TestData.Account);
-                dbContext.SaveChanges();
+                _dbContext.Courses.Add(TestData.Course);
             }
         }
 
-        private void ConfigureCustomValueRetrievers()
+        private void AddAccountsToDb()
         {
-            Service.Instance.ValueRetrievers.Replace<DateTimeValueRetriever, CustomDateTimeValueRetriever>();
-            Service.Instance.ValueRetrievers.Replace<NullableDateTimeValueRetriever, CustomNullableDateTimeValueRetriever>();
-            Service.Instance.ValueRetrievers.Replace<ShortValueRetriever, CustomShortValueRetriever>();
+            var accounts = new List<Account>
+            {
+                TestData.Account,
+                new Account {Id = AccountId + 1, Name = "Account 2 non-levy"},
+                new Account {Id = AccountId + 10, Name = "Account 11 levy", IsLevy = true},
+                new Account {Id = AccountId + 11, Name = "Account 12 levy", IsLevy = true}
+            };
+
+            foreach (var account in accounts)
+            {
+                if(_dbContext.Accounts.Find(account.Id) != null)
+                    continue;
+
+                _dbContext.Accounts.Add(account);
+                _dbContext.SaveChanges();
+            }
         }
 
-        private void ConfigureCustomValueComparers()
+        private void AddAccountLegalEntitiesToDb()
         {
-            Service.Instance.ValueComparers.Register<CustomShortValueComparer>();
-            Service.Instance.ValueComparers.Register(new CustomDateTimeComparer(new CustomDateTimeValueRetriever()));
+            var accountLegalEntities = new List<AccountLegalEntity>
+            {
+                TestData.AccountLegalEntity,
+                new AccountLegalEntity
+                {
+                    AccountId = AccountId + 1,
+                    AccountLegalEntityId = 2,
+                    AccountLegalEntityName = "ALE 2 - non levy",
+                    AgreementSigned = true
+                },
+                new AccountLegalEntity
+                {
+                    AccountId = AccountId + 10,
+                    AccountLegalEntityId = 10,
+                    AccountLegalEntityName = "ALE 10 - levy",
+                    AgreementSigned = true,
+                },
+                new AccountLegalEntity
+                {
+                    AccountId = AccountId + 11,
+                    AccountLegalEntityId = 20,
+                    AccountLegalEntityName = "ALE 20 - levy",
+                    AgreementSigned = true
+                },
+                new AccountLegalEntity
+                {
+                    AccountId = AccountId + 1,
+                    AccountLegalEntityId = AccountLegalEntityId + 100,
+                    AccountLegalEntityName = "Pass Mark Corp",
+                    AgreementSigned = true
+                }
+            };
+
+            foreach (var accountLegalEntity in accountLegalEntities)
+            {
+                if(_dbContext.AccountLegalEntities.SingleOrDefault(e => 
+                       e.AccountLegalEntityId.Equals(accountLegalEntity.AccountLegalEntityId)) != null)
+                    continue;
+
+                _dbContext.AccountLegalEntities.Add(accountLegalEntity);
+                _dbContext.SaveChanges();
+            }
         }
     }
 }
