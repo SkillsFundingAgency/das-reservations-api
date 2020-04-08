6215d3316e93cdcaa30e812cf2845ac505d485f8
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-04-08 17:39:10 +0100

    Add new mapping from Entity to domain model
    Added new mapping for the Account entity to the domain model. It will use the globalreservationlimit if the entity has null for its reservation limit

--- src/SFA.DAS.Reservations.Domain/Account/Account.cs
+++ src/SFA.DAS.Reservations.Domain/Account/Account.cs
@@ -2,17 +2,25 @@ namespace SFA.DAS.Reservations.Domain.Account
 {
     public class Account
     {
-        public long Id { get ; set ; }
-        public bool IsLevy { get ; set ; }
-        public string Name { get ; set ; }
-        public int? ReservationLimit { get; set; }
+        public long Id { get ;  }
+        public bool IsLevy { get ;  }
+        public string Name { get ;  }
+        public int ReservationLimit { get;  }
 
+        public Account(Domain.Entities.Account account, int globalReservationLimit)
+        {
+            Id = account.Id;
+            IsLevy = account.IsLevy;
+            Name = account.Name;
+            ReservationLimit = account.ReservationLimit ?? globalReservationLimit;
+        }
+        
         public Account(long id, bool isLevy, string name, int? reservationLimit)
         {
             Id = id;
             IsLevy = isLevy;
             Name = name;
-            ReservationLimit = reservationLimit;
+            ReservationLimit = reservationLimit ?? 0;
         }
 
         public Account()
