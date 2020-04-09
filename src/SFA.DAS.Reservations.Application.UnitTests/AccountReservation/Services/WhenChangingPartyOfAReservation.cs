750514f6c5dc81dff3ef9c32f4d47d4ad8aed92f
Scott Wakefield <scott@expertcc.co.uk>
2020-04-09 19:04:57 +0100

    MF-507 added test for deleted status.

--- src/SFA.DAS.Reservations.Application.UnitTests/AccountReservation/Services/WhenChangingPartyOfAReservation.cs
+++ src/SFA.DAS.Reservations.Application.UnitTests/AccountReservation/Services/WhenChangingPartyOfAReservation.cs
@@ -30,13 +30,33 @@ namespace SFA.DAS.Reservations.Application.UnitTests.AccountReservation.Services
         }
 
         [Test, RecursiveMoqAutoData]
-        public void And_Reservation_Not_Confirmed_Status_Then_Throws_ArgumentException(
+        public void And_Reservation_Completed_Status_Then_Throws_ArgumentException(
             ChangeOfPartyServiceRequest request,
             Domain.Entities.Reservation existingReservation,
             [Frozen] Mock<IReservationRepository> mockRepository,
             AccountReservationService service)
         {
             existingReservation.Status = (short) ReservationStatus.Completed;
+            mockRepository
+                .Setup(repository => repository.GetById(request.ReservationId))
+                .ReturnsAsync(existingReservation);
+
+            var act = new Func<Task>(async () => await service.ChangeOfParty(request));
+
+            act.Should().Throw<ArgumentException>()
+                .Where(exception => 
+                    exception.ParamName == nameof(ChangeOfPartyServiceRequest.ReservationId) &&
+                    exception.Message.StartsWith("Reservation cannot be changed due to it's status."));
+        }
+
+        [Test, RecursiveMoqAutoData]
+        public void And_Reservation_Deleted_Status_Then_Throws_ArgumentException(
+            ChangeOfPartyServiceRequest request,
+            Domain.Entities.Reservation existingReservation,
+            [Frozen] Mock<IReservationRepository> mockRepository,
+            AccountReservationService service)
+        {
+            existingReservation.Status = (short) ReservationStatus.Deleted;
             mockRepository
                 .Setup(repository => repository.GetById(request.ReservationId))
                 .ReturnsAsync(existingReservation);
