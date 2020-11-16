506bc0bb4b421255032ad3d06fd3181a9a53d50b
jack-education <38191888+jack-education@users.noreply.github.com>
2020-11-16 16:42:53 +0000

    Revert "Removed unneeded ValueRetriever replacement"
    This reverts commit eb1f10d144fb401bc9588ae531f9f131f42c9f8d.

--- src/SFA.DAS.Reservations.Api.AcceptanceTests/Steps/StepsBase.cs
+++ src/SFA.DAS.Reservations.Api.AcceptanceTests/Steps/StepsBase.cs
@@ -50,6 +50,7 @@ namespace SFA.DAS.Reservations.Api.AcceptanceTests.Steps
         private void ConfigureCustomValueRetrievers()
         {
             Service.Instance.ValueRetrievers.Replace<DateTimeValueRetriever, CustomDateTimeValueRetriever>();
+            Service.Instance.ValueRetrievers.Replace<CustomNullableDateTimeValueRetriever, CustomNullableDateTimeValueRetriever>();
             Service.Instance.ValueRetrievers.Replace<ShortValueRetriever, CustomShortValueRetriever>();
         }
 
