f185efdea9a09c0d5fdffe784f04d0a9fb151cf9
VasanthaKasirajan3008 <56582101+VasanthaKasirajan3008@users.noreply.github.com>
2021-11-11 10:00:23 +0000

    CON-4082

--- src/SFA.DAS.Reservations.Api/StartupExtensions/EntityFrameworkStartup.cs
+++ src/SFA.DAS.Reservations.Api/StartupExtensions/EntityFrameworkStartup.cs
@@ -3,24 +3,32 @@ using Microsoft.Extensions.DependencyInjection;
 using NServiceBus.Persistence;
 using SFA.DAS.NServiceBus.SqlServer.Data;
 using SFA.DAS.Reservations.Data;
-using SFA.DAS.UnitOfWork;
 using SFA.DAS.UnitOfWork.Context;
+using System.Collections.Generic;
 
 namespace SFA.DAS.Reservations.Api.StartupExtensions
 {
     public static class EntityFrameworkStartup
     {
-        public static IServiceCollection AddEntityFramework(this IServiceCollection services)
+        public static IServiceCollection AddEntityFramework(this IServiceCollection services, Microsoft.Extensions.Options.IOptions<Domain.Configuration.ReservationsConfiguration> config)
         {
             return services.AddScoped(p =>
             {
                 var unitOfWorkContext = p.GetService<IUnitOfWorkContext>();
-                var synchronizedStorageSession = unitOfWorkContext.Get<SynchronizedStorageSession>();
-                var sqlStorageSession = synchronizedStorageSession.GetSqlStorageSession();
-                var optionsBuilder = new DbContextOptionsBuilder<ReservationsDataContext>().UseSqlServer(sqlStorageSession.Connection);
-                var dbContext = new ReservationsDataContext(optionsBuilder.Options);
-
-                dbContext.Database.UseTransaction(sqlStorageSession.Transaction);
+                ReservationsDataContext dbContext;
+                try
+                {                    
+                    var synchronizedStorageSession = unitOfWorkContext.Get<SynchronizedStorageSession>();
+                    var sqlStorageSession = synchronizedStorageSession.GetSqlStorageSession();
+                    var optionsBuilder = new DbContextOptionsBuilder<ReservationsDataContext>().UseSqlServer(sqlStorageSession.Connection);
+                    dbContext = new ReservationsDataContext(optionsBuilder.Options);
+                    dbContext.Database.UseTransaction(sqlStorageSession.Transaction);
+                }
+                catch (KeyNotFoundException)
+                {
+                    var optionsBuilder = new DbContextOptionsBuilder<ReservationsDataContext>().UseSqlServer(config.Value.ConnectionString);
+                    dbContext = new ReservationsDataContext(optionsBuilder.Options);
+                }
 
                 return dbContext;
             });
