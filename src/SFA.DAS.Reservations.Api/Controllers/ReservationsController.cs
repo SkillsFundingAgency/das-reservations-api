9a839ac6a36ec4e56546303ea839b177a57e128a
Paul Graham <paul.graham@coprime.co.uk>
2024-09-11 09:57:17 +0100

    removed usused ref

--- src/SFA.DAS.Reservations.Api/Controllers/ReservationsController.cs
+++ src/SFA.DAS.Reservations.Api/Controllers/ReservationsController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Azure;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
