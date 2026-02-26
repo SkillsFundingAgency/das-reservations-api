415e5ec602c569f15f66b23a2e73fe4e4ca9b42c
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2026-02-26 22:02:10 +0000

    tidy

--- src/SFA.DAS.Reservations.Application/Rules/Services/AvailableDatesService.cs
+++ src/SFA.DAS.Reservations.Application/Rules/Services/AvailableDatesService.cs
@@ -1,27 +1,25 @@
-using System;
 using System.Collections.Generic;
 using Microsoft.Extensions.Options;
 using SFA.DAS.Reservations.Domain.Configuration;
 using SFA.DAS.Reservations.Domain.Rules;
 
-namespace SFA.DAS.Reservations.Application.Rules.Services
+namespace SFA.DAS.Reservations.Application.Rules.Services;
+
+public class AvailableDatesService(IOptions<ReservationsConfiguration> options, ICurrentDateTime currentDateTime)
+    : IAvailableDatesService
 {
-    public class AvailableDatesService(IOptions<ReservationsConfiguration> options, ICurrentDateTime currentDateTime)
-        : IAvailableDatesService
+    private readonly ReservationsConfiguration _configuration = options.Value;
+
+    public IEnumerable<AvailableDateStartWindow> GetAvailableDates(bool includePreviousMonth = true)
     {
-        private readonly ReservationsConfiguration _configuration = options.Value;
+        var now = currentDateTime.GetDate();
 
-        public IEnumerable<AvailableDateStartWindow> GetAvailableDates(bool includePreviousMonth = true)
-        {
-            var now = currentDateTime.GetDate();
-
-            return new AvailableDates(
+        return new AvailableDates(
                 now,
                 _configuration.NumberOfAvailableDates,
                 _configuration.AvailableDatesMinDate,
                 _configuration.AvailableDatesMaxDate,
                 includePreviousMonth)
-                .Dates;
-        }
+            .Dates;
     }
 }
